a78fb5d74cf64ab2b8d3e4bb806b602ada1796ab
phryszlo <phryszlo@gmail.com>
2022-09-03 10:53:51 -0400

    cleaned up, removed many previously useful loggings

--- app.js
+++ app.js
@@ -1,59 +1,48 @@
-// import twoLetterWords from "./wordlists/json/2-letter-words.json";
-// import threeLetterWords from "./wordlists/json/3-letter-words.json";
-// import fourLetterWords from "./wordlists/4-letter-words.json.js";
-// import fiveLetterWords from "./wordlists/5-letter-words.json.js";
-// import sixLetterWords from "./wordlists/6-letter-words.json.js";
-// import sevenLetterWords from "./wordlists/7-letter-words.json.js";
-// import eightLetterWords from "./wordlists/8-letter-words.json.js";
-// import nineLetterWords from "./wordlists/9-letter-words.json.js";
-// import tenLetterWords from "./wordlists/10-letter-words.json.js";
-// import elevenLetterWords from "./wordlists/11-letter-words.json.js";
-// import twelveLetterWords from "./wordlists/12-letter-words.json.js";
-// import thirteenLetterWords from "./wordlists/13-letter-words.json.js";
-// import fourteenLetterWords from "./wordlists/14-letter-words.json.js";
-// import fifteenLetterWords from "./wordlists/15-letter-words.json.js";
+
 import spacelog from "./spaceLog.js";
 import wordLists from "./wordLists.js"
 
-// const objWordlists = {
-//   2: twoLetterWords,
-//   3: threeLetterWords,
-//   4: fourLetterWords,
-//   5: fiveLetterWords,
-//   6: sixLetterWords,
-//   7: sevenLetterWords,
-//   8: eightLetterWords,
-//   9: nineLetterWords,
-//   10: tenLetterWords,
-//   11: elevenLetterWords,
-//   12: twelveLetterWords,
-//   13: thirteenLetterWords,
-//   14: fourteenLetterWords,
-//   15: fifteenLetterWords
-// }
-// const wordlists = [
-//   twoLetterWords,
-//   threeLetterWords,
-//   JSON.stringify(fourLetterWords),
-//   JSON.stringify(fiveLetterWords),
-//   JSON.stringify(sixLetterWords),
-//   JSON.stringify(sevenLetterWords),
-//   JSON.stringify(eightLetterWords),
-//   JSON.stringify(nineLetterWords),
-//   JSON.stringify(tenLetterWords),
-//   JSON.stringify(elevenLetterWords),
-//   JSON.stringify(twelveLetterWords),
-//   JSON.stringify(thirteenLetterWords),
-//   JSON.stringify(fourteenLetterWords),
-//   JSON.stringify(fifteenLetterWords)
-// ]
+const fullWordList = [];
 
 window.addEventListener('load', async () => {
-  spacelog(
-    await wordLists()
-      .then(json => {
-        return json.twoLetterWords[0].word
+  await wordLists()
+    .then(arrWordLists => {
+
+      // #region .... some historical notes ....
+
+      // spacelog(Object.keys(arrWordLists[1])[0])
+      // // so that's the keyname of the array in the json
+      // // e.g. "threeLetterWords"
+
+      // // spacelog(Object.values(arrWordLists[1])[0])
+      // // and that's the whole array of objects under key "threeLetterWords"
+
+      // spacelog(Object.values(arrWordLists[1])[0][47].word)
+      // // and that's the value with key "word" for index 47 
+      // // of the object array under "threeLetterWords"
+
+      // #endregion
+
+
+      // wordLists.js returned an array of the response.json() of each .json file
+      // forEach full json object (i.e. response.json())
+      arrWordLists.forEach(async (list, idx) => {
+        spacelog(Object.keys(list)[0]) // the main keyname e.g. "threeLetterWords"
+
+        spacelog(Object.values(list).length)
+
+        // forEach {word: value} object in the list (: array)
+        Object.values(list)[0].forEach(async (objWord, i) => {
+          // spacelog(objWord.word)
+          // and THAT contains the word. and if you uncomment that,
+          // it will take 5 minutes to log all 14 arrays of thousands of words (170,000+)
+          // --> so don't.
+
+          fullWordList.push(objWord.word)
+        })
       })
-  )
-})
+    })
+  spacelog(`'done': fullWordList contains ${fullWordList.length} words. fullWordList[6000] = ${fullWordList[6000]}`)
+
+}) // end window.addEvenListener('load')
 
--- wordLists.js
+++ wordLists.js
@@ -1,25 +1,66 @@
-const parseWordLists = () => {
-  fetch("./wordlists/json/2-letter-words.json")
-    .then(response => {
-      return response.json();
-    })
-    .then(
-      data =>
-          JSON.parse(
-            JSON.stringify(data.twoLetterWords)
-          )
-    ).then(
-      data => {return data}
-    )
-}
+import spacelog from "./spaceLog.js";
 
-// const wordLists = parseWordLists()
-
+const allWordLists = []
 const wordLists = async () => {
-  const response = await fetch("./wordlists/json/2-letter-words.json")
-  const twoLetterWords = await response.json();
-  return twoLetterWords
-}
 
+  let response = await fetch("./wordlists/json/2-letter-words.json")
+  const twoLetterWords = await response.json();
+  allWordLists.push(twoLetterWords)
+
+  response = await fetch("./wordlists/json/3-letter-words.json")
+  const threeLetterWords = await response.json()
+  allWordLists.push(threeLetterWords)
+
+  response = await fetch("./wordlists/json/4-letter-words.json")
+  const fourLetterWords = await response.json();
+  allWordLists.push(fourLetterWords)
+
+  response = await fetch("./wordlists/json/5-letter-words.json")
+  const fiveLetterWords = await response.json();
+  allWordLists.push(fiveLetterWords)
+
+  response = await fetch("./wordlists/json/6-letter-words.json")
+  const sixLetterWords = await response.json();
+  allWordLists.push(sixLetterWords)
+
+  response = await fetch("./wordlists/json/7-letter-words.json")
+  const sevenLetterWords = await response.json();
+  allWordLists.push(sevenLetterWords)
+
+  response = await fetch("./wordlists/json/8-letter-words.json")
+  const eightLetterWords = await response.json();
+  allWordLists.push(eightLetterWords)
+
+  response = await fetch("./wordlists/json/9-letter-words.json")
+  const nineLetterWords = await response.json();
+  allWordLists.push(nineLetterWords)
+
+  response = await fetch("./wordlists/json/10-letter-words.json")
+  const tenLetterWords = await response.json();
+  allWordLists.push(tenLetterWords)
+
+  response = await fetch("./wordlists/json/11-letter-words.json")
+  const elevenLetterWords = await response.json();
+  allWordLists.push(elevenLetterWords)
+
+  response = await fetch("./wordlists/json/12-letter-words.json")
+  const twelveLetterWords = await response.json();
+  allWordLists.push(twelveLetterWords)
+
+  response = await fetch("./wordlists/json/13-letter-words.json")
+  const thirteenLetterWords = await response.json();
+  allWordLists.push(thirteenLetterWords)
+
+  response = await fetch("./wordlists/json/14-letter-words.json")
+  const fourteenLetterWords = await response.json();
+  allWordLists.push(fourteenLetterWords)
+
+  response = await fetch("./wordlists/json/15-letter-words.json")
+  const fifteenLetterWords = await response.json();
+  allWordLists.push(fifteenLetterWords)
+
+
+  return allWordLists
+}
 
 export default wordLists
