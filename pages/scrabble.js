bd0f5d67575ae3977aed74e8df7820abab77dbc0
phryszlo <phryszlo@gmail.com>
2022-09-03 20:26:15 -0400

    due for a commit. layout progressing. legend in. trays beginning.

--- pages/scrabble.js
+++ pages/scrabble.js
@@ -1,11 +1,12 @@
 
 import spacelog from "../spaceLog.js";
 import wordLists from "./wordLists.js"
-import navbar from "../navbar.js"
+// import navbar from "../navbar.js"
 
 // HTML element refs
 const gameboard = document.querySelector('.gameboard');
 
+// the four special square-types (triple-word-score, double-letter-score, etc.)
 const tws = [16513, 0, 0, 0, 0, 0, 0, 16513, 0, 0, 0, 0, 0, 0, 16513]
 const dws = [0, 8194, 4100, 2056, 1040, 0, 0, 128, 0, 0, 1040, 2056, 4100, 8194, 0]
 const tls = [0, 544, 0, 0, 0, 8738, 0, 0, 0, 8738, 0, 0, 0, 544, 0]
@@ -21,13 +22,12 @@ const getDiv = (className = 'div') => {
 }
 
 const getOneRow = (idx, valsAtIdx) => {
-  // valsAtIdx is a four-element array (of arrays)
+  // valsAtIdx is a four-element array (of arrays): the four special square-types
   let row = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   valsAtIdx.forEach((val, index) => {
 
     // translate the decimal val into binary number (string) with leading '0's
     const bits = val.toString(2).padStart(15, '0')
-    spacelog(bits)
     if (bits === '0') {
       row[bit] = getDiv('')
     } else {
@@ -71,77 +71,23 @@ const loadBoard = () => {
   board.forEach(row => {
     row.forEach(square => {
       spacelog(square.classList)
+      gameboard.append(square)
     })
   })
-  // spacelog(board[4][2].classList)
-  // spacelog(board)
 }
 
-// const getDwsDiv = () => {
-//   const dwsDiv = document.createElement('div')
-//   dwsDiv.classList.add('dws-div')
-//   return dwsDiv
-// }
-// const getTlsDiv = () => {
-//   const tlsDiv = document.createElement('div')
-//   tlsDiv.classList.add('tls-div')
-//   return tlsDiv
-// }
-// const getDlsDiv = () => {
-//   const dlsDiv = document.createElement('div')
-//   dlsDiv.classList.add('dls-div')
-//   return dlsDiv
-// }
 
-
-
-/*
-tws.forEach(num => {
-  if (num != 0) {
-    let binary = num.toString(2)
-
-    for (let bit = 0; bit < binary.length; bit++) {
-      if (binary[bit] === '1') console.log('TWS')
-      else console.log('_');
-    }
-  }
-  else {
-    console.log('_ _ _ _ _ _ _ _ _ _ _ _ _ _ _');
-  }
-})
-*/
 
 
 window.addEventListener('load', async () => {
   await wordLists()
     .then(arrWordLists => {
 
-      // #region .... some historical notes ....
-
-      // spacelog(Object.keys(arrWordLists[1])[0])
-      // // so that's the keyname of the array in the json
-      // // e.g. "threeLetterWords"
-
-      // // spacelog(Object.values(arrWordLists[1])[0])
-      // // and that's the whole array of objects under key "threeLetterWords"
-
-      // spacelog(Object.values(arrWordLists[1])[0][47].word)
-      // // and that's the value with key "word" for index 47 
-      // // of the object array under "threeLetterWords"
-
-      // #endregion
-
-
       // wordLists.js returned an array of the response.json() of each .json file
-      // forEach full json object (i.e. response.json())
       arrWordLists.forEach(async (list, idx) => {
 
         // forEach {word: value} object in the list (: array)
         Object.values(list)[0].forEach(async (objWord, i) => {
-          // spacelog(objWord.word)
-          // and THAT contains the word. and if you uncomment that,
-          // it will take 5 minutes to log all 14 arrays of thousands of words (170,000+)
-          // --> so don't.
 
           fullWordList.push(objWord.word)
         })
