bea572420bafb78dcc05fe20907ed2220c44d414
phryszlo <phryszlo@gmail.com>
2022-09-04 14:10:43 -0400

    tile creation loop working. some comment updating.

--- pages/scrabble.js
+++ pages/scrabble.js
@@ -18,16 +18,67 @@ const dws = [0, 8194, 4100, 2056, 1040, 0, 0, 128, 0, 0, 1040, 2056, 4100, 8194,
 const tls = [0, 544, 0, 0, 0, 8738, 0, 0, 0, 8738, 0, 0, 0, 544, 0]
 const dls = [2056, 0, 320, 16513, 0, 0, 4420, 2056, 4420, 0, 0, 16513, 320, 0, 2056]
 
-
+// to contain the entire word list as parsed from the 14 json files
+// MAYBE TODO: do separate arrays for each word-length (to prevent find()s on word => word.length === len)
+// the array will contain like 170,000 words
 const fullWordList = [];
 
 
+/*
+TILE DISTRIBUTION (official)
+(1 point)-A, E, I, O, U, L, N, S, T, R
+(2 points)-D, G
+(3 points)-B, C, M, P
+(4 points)-F, H, V, W, Y
+(5 points)-K
+(8 points)- J, X
+(10 points)-Q, Z
+*/
+
+const tileDistribution = [
+  [],
+  ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R'],
+  ['D', 'G'],
+  ['B', 'C', 'M', 'P'],
+  ['F', 'H', 'V', 'W', 'Y'],
+  ['K'],
+  [],
+  [],
+  ['J', 'X'],
+  [],
+  ['Q', 'Z']
+]
+
+const bagOfTiles = []
+
 const createAllTiles = () => {
-  let tile;
+  let tile
   for (let i = 1; i < 200; i++) {
     tile = document.createElement('div')
     tile.classList.add('tile')
   }
+
+  tileDistribution.forEach((arrLetters, idx) => {
+    arrLetters.forEach((letter) => {
+      // idx is the point val
+      tile = document.createElement('div')
+      tile.classList.add('tile', `letter-${letter}`, `points-${idx}`)
+      tile.style.backgroundImage = `url('images/tiles/${letter}.png)`
+      bagOfTiles.push(tile)
+      spacelog(tile.classList);
+    })
+
+    // push a couple of blanks
+    for (let i = 1; i <= 2; i++) {
+      tile = document.createElement('div')
+      tile.classList.add('tile', `letter-blank`)
+      tile.style.backgroundImage = `url('images/tiles/blank.png)`
+      bagOfTiles.push(tile)
+      spacelog(tile.classList);
+
+    }
+  })
+
 
 }
 
@@ -113,29 +164,31 @@ function handleDrop(e) {
 
 
 
-
-
-
 const getDiv = (className = 'div') => {
   const div = document.createElement('div')
-  div.classList.add('square', className)
+  div.classList.add('square', className, `row-`)
 
   return div
 }
 
 const getOneRow = (idx, valsAtIdx) => {
-  // valsAtIdx is a four-element array (of arrays): the four special square-types
-  let row = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  valsAtIdx.forEach((val, index) => {
 
+  // there are 4 vals at each idx: we have 4 arrays of 15 elements. idx = 0-14, valsAtIdx = 0-3.
+
+  let row = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+  // valsAtIdx is a four-element array: one for each of the four special square-types
+  // think: the vertical cross of the four 15-element arrays stacked (a 4 high, 15 across grid)
+  valsAtIdx.forEach((val, index) => {
+    // val is the actual number in the row, index is the position in the row
     // translate the decimal val into binary number (string) with leading '0's
     const bits = val.toString(2).padStart(15, '0')
     if (bits === '0') {
-      row[bit] = getDiv('')
+      row[index] = getDiv('')
     } else {
       for (let bit = 0; bit < bits.length; bit++) {
 
-        // idx here is WHICH SQUARE-TYPE (i.e., idx=0 means square-type is TWS)
+        // idx here is WHICH SQUARE-TYPE (e.g., idx=0 means square-type is TripleWordScore(TWS))
         // so each switch will be hit 15 TIMES in a row before moving to the next case
         switch (index) {
           case 0:
@@ -168,11 +221,11 @@ const loadBoard = () => {
     board.push(getOneRow(i, currentRow))
   }
 
-  spacelog(`board.length = ${board.length}`)
+  // spacelog(`board.length = ${board.length}`)
 
   board.forEach(row => {
     row.forEach(square => {
-      spacelog(square.classList)
+      // spacelog(square.classList)
       gameboard.append(square)
 
       // I am leaving these listener adds here because it was not working when I had them after loadBoard in DOMContentLoaded
@@ -205,7 +258,7 @@ window.addEventListener('DOMContentLoaded', async () => {
       })
     })
   spacelog(`'done': fullWordList contains ${fullWordList.length} words. fullWordList[6000] = ${fullWordList[6000]}`)
-  spacelog(`fullWordList[99999] = ${fullWordList[99999]}`)
+  spacelog(`fullWordList[11999] = ${fullWordList[11999]}`)
   loadBoard();
 
   // loadBoard() has called getOneRow() which calls getDiv(), where the .square class is added
@@ -214,7 +267,7 @@ window.addEventListener('DOMContentLoaded', async () => {
   const squares = document.querySelectorAll('.square');
 
 
-
+  createAllTiles();
 
 
 
